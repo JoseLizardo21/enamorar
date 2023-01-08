d26e04e5e8c6030c1d8ae15b033a93f93fc6a1c5
Jose <jose.lizardo21.cont@gmail.com>
2023-01-08 15:02:57 -0500

    n

--- keys.js
+++ keys.js
@@ -1,8 +1,11 @@
+if(process.env.NODE_ENV != 'production'){
+    require('dotenv').config()
+}
 module.exports = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database:'enamorar'
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE
     }
 }
